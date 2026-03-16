84e58189efe40b4807a677e58666bb83ef8d6aaa
codyingyao <163702764+codyingyao@users.noreply.github.com>
2026-03-16 22:34:13 +0000

    fixed the issues with gpu acceleration

--- run_sim.sh
+++ run_sim.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# Runs your python simulation with GPU support.
-# Replace 'run_simulation.py' with your actual python script.
-# "$@" passes any CLI flags right through to python.
-apptainer run --nv smiles.sif python src/run_aamd.py "$@"
+sudo apptainer run --nvccli smiles.sif python src/run_aamd.py "$@"
